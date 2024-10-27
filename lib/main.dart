0a68c754d606f7fb156c4a3d0ae46fd112d7f8d1
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-10-28 05:23:46 +0530

    ticket box designed

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,22 @@ import 'package:ticket_app/base/button_nav_bar.dart';
 
 void main() { //main entry point
   runApp(const MyApp()); //framework entry point
-  var test = Test(x:1,y:2);
-  print(test.x);
-  print(test.y);
+  // var test = Test(x:1,y:2);
+  // print(test.x);
+  // print(test.y);
 
-  var newTest = test.copyWith(x: 100);
-  print(newTest.x);
-  print(newTest.y);
+  // var newTest = test.copyWith(x: 100);
+  // print(newTest.x);
+  // print(newTest.y);
 }
 
-class Test{
-  int x,y;
-  Test({required this.x, required this.y});
-  Test copyWith({int? x,int? y}){
-    return Test(x:x??this.x,y:y??this.y);
-  }
-}
+// class Test{
+//   int x,y;
+//   Test({required this.x, required this.y});
+//   Test copyWith({int? x,int? y}){
+//     return Test(x:x??this.x,y:y??this.y);
+//   }
+// }
 //every class is a widget
 
 class MyApp extends StatelessWidget { //stateless means no activity or dead
